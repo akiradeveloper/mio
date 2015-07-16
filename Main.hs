84b2cb8f2a9ae0605ff725cd391b003e3d1324b6
Akira Hayakawa <ruby.wktk@gmail.com>
2015-07-16 18:10:05 +0900

    work
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- Main.hs
+++ Main.hs
@@ -4,6 +4,8 @@ import System.IO
 import Control.Monad
 import Data.Bits
 import Data.ByteString
+import System.Random
+import Data.Word
 
 data Direction = R | W deriving Show               
 type IOLine = (Direction, Int, Int)
@@ -17,20 +19,31 @@ split s =
      read b :: Int,
      read c :: Int)
 
+randomByteString n g = pack $ randomBytes n g
+  where randomBytes 0 _ = []
+        randomBytes n g = fromIntegral val:randomBytes (n-1) nextG
+          where (val, nextG) = next g
+-- randomByteString n g = undefined
+
 main = do
     [o, m] <- getArgs
     lines <- fmap (fmap Main.split . lines) System.IO.getContents
-    origH <- openFile o ReadMode
+    origH <- openFile o ReadWriteMode
     mirrH <- openFile m ReadWriteMode
     forM_ lines $ \(rw, start, len) -> do
       case rw of
         R -> do
           hSeek origH AbsoluteSeek (toInteger $ start `shiftL` 9)
           origDat <- hGet origH (len `shiftL` 9)
-          mirrDat <- hGet mirrH (len `shiftL` 9)
-          print $ if origDat == mirrDat then "" else "read fail"
-        W -> do
-          dat <- undefined
-          hSeek origH AbsoluteSeek (toInteger $ start `shiftL` 9)
           hSeek mirrH AbsoluteSeek (toInteger $ start `shiftL` 9)
-          print "write"
+          mirrDat <- hGet mirrH (len `shiftL` 9)
+          if origDat == mirrDat then return () 
+                                else print "read fail"
+        W -> do
+          g <- getStdGen
+          dat <- return $ randomByteString (len `shiftL` 9) g
+          hSeek origH AbsoluteSeek (toInteger $ start `shiftL` 9)
+          hPut origH dat
+          hSeek mirrH AbsoluteSeek (toInteger $ start `shiftL` 9)
+          hPut mirrH dat
+          return ()
